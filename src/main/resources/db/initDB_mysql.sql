f98fad01d83de3e5b940ec938fd9c520c70f63e4
AndrePro <protasenya.andre@mail.ru>
2017-01-17 22:35:57 +0300

    populate DB

---
+++ src/main/resources/db/initDB_mysql.sql
@@ -0,0 +1,20 @@
+CREATE TABLE users
+(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    registered TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    calories_per_day INT DEFAULT '2000'
+);
+CREATE UNIQUE INDEX unique_email ON users (email);
+
+ALTER TABLE users AUTO_INCREMENT = 100000;
+
+
+CREATE TABLE user_roles
+(
+  user_id INTEGER NOT NULL,
+  role    VARCHAR(50),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
---
+++ src/main/resources/db/populateDB_mysql.sql
@@ -0,0 +1,9 @@
+-- password
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password');
+-- admin
+INSERT INTO users (name, email, password)
+VALUES ('Admin', 'admin@gmail.com', 'admin');
+
+INSERT INTO user_roles (role, user_id) VALUES ('ROLE_USER', 100000);
+INSERT INTO user_roles (role, user_id) VALUES ('ROLE_ADMIN', 100001);
